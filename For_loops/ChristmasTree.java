5d2ce06ed935a5273f76950bcd40ab0bcfbff518
EminMOS <emi.senguel.24@lehre.mosbach.dhbw.de>
2024-12-20 11:07:54 +0100

    Christmas Tree

---
+++ For_loops/ChristmasTree.java
@@ -0,0 +1,33 @@
+public class ChristmasTree {
+    public static void main(String[] args) {
+        int height = 5; // Height of the tree
+
+        // Top of the tree
+        System.out.println("    +");
+
+        // Tree body with stars
+        for (int i = 1; i <= height; i++) {
+            // Print spaces to center the stars
+            for (int j = 1; j <= height - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+
+            // Move to the next line for the next tree layer
+            System.out.println();
+        }
+
+        // Tree trunk
+        for (int i = 1; i <= 2; i++) {
+            // Print spaces to center the trunk
+            for (int j = 1; j < height; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("***");
+        }
+    }
+}
